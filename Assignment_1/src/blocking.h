7dad083606c3b646c8462929564894f657f8e5dd
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-02 23:14:58 +0530

    Add header file for gemmBlocking function

--- Assignment_1/src/blocking.h
+++ Assignment_1/src/blocking.h
@@ -1,1 +1,17 @@
+#ifndef BLOCKING_H
+#define BLOCKING_H
 
+#include <vector>
+using namespace std;
+
+void gemmBlocking(
+    const vector<vector<int>>& A,
+    const vector<vector<int>>& B,
+    vector<vector<int>>& C,
+    int m,
+    int k,
+    int n,
+    int blockSize
+);
+
+#endif
