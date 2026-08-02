5b496cc24f9a618e72773725e641f94d9ef2710f
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-02 23:15:20 +0530

    Implement simple matrix multiplication function

--- Assignment_1/src/simple.cpp
+++ Assignment_1/src/simple.cpp
@@ -1,1 +1,25 @@
+#include "simple.h"
 
+void gemmSimple(
+    const vector<vector<int>>& A,
+    const vector<vector<int>>& B,
+    vector<vector<int>>& C,
+    int M,
+    int K,
+    int N)
+{
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            int sum = 0;
+
+            for(int k = 0; k < K; k++)
+            {
+                sum += A[i][k] * B[k][j];
+            }
+
+            C[i][j] = sum;
+        }
+    }
+}
