c4e5cc230dc6cf2c663ab843f42c71f8fd84d7ca
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-02 23:15:38 +0530

    Add header file for gemmSimple function

--- Assignment_1/src/simple.h
+++ Assignment_1/src/simple.h
@@ -1,1 +1,16 @@
+#ifndef SIMPLE_H
+#define SIMPLE_H
 
+#include <vector>
+using namespace std;
+
+void gemmSimple(
+    const vector<vector<int>>& A,
+    const vector<vector<int>>& B,
+    vector<vector<int>>& C,
+    int M,
+    int K,
+    int N
+);
+
+#endif
