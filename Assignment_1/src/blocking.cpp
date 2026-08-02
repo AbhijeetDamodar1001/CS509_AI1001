6a6b62b5c50d8835843799429605228bf665c08b
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-02 23:14:20 +0530

    Add gemmBlocking function for optimized matrix multiplication
    Implement matrix multiplication using blocking technique.

--- Assignment_1/src/blocking.cpp
+++ Assignment_1/src/blocking.cpp
@@ -1,1 +1,41 @@
+#include "blocking.h"
+#include <vector>
+#include <algorithm>
 
+void gemmBlocking(
+    const vector<vector<int>>& A,
+    const vector<vector<int>>& B,
+    vector<vector<int>>& C,
+    int m,
+    int k,
+    int n,
+    int blockSize)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            C[i][j] = 0;
+        }
+    }
+
+    for(int ii = 0; ii < m; ii += blockSize)
+    {
+        for(int jj = 0; jj < n; jj += blockSize)
+        {
+            for(int kk = 0; kk < k; kk += blockSize)
+            {
+                for(int i = ii; i < min(ii + blockSize, m); i++)
+                {
+                    for(int j = jj; j < min(jj + blockSize, n); j++)
+                    {
+                        for(int x = kk; x < min(kk + blockSize, k); x++)
+                        {
+                            C[i][j] += A[i][x] * B[x][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
