4c7fbdcb316f14decdc5df0048c4bf758d884637
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-05 22:19:23 +0530

    Add header file for simple matrix multiplication

---
+++ Assignment_1/GEMM/src/simple.h
@@ -0,0 +1,14 @@
+#ifndef SIMPLE_H
+#define SIMPLE_H
+
+#include <vector>
+
+using namespace std;
+
+
+vector<vector<int>> simpleMultiply(
+    const vector<vector<int>>& matrixA,
+    const vector<vector<int>>& matrixB
+);
+
+#endif
