7cc67441750396ff326a32445625948d4036281f
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-05 22:15:18 +0530

    Add convert_to_csr function for CSR conversion
    Implement function to convert adjacency list to CSR format.

--- Assignment_1/CSR/src/csr.cpp
+++ Assignment_1/CSR/src/csr.cpp
@@ -1,1 +1,22 @@
+#include "csr.h"
 
+csrgraph convert_to_csr(const vector<vector<pair<int, int>>>& adjacencyList)
+{
+    csrgraph csr;
+
+    csr.rowPtr.push_back(0);
+
+
+    for (int i = 0; i < adjacencyList.size(); i++)
+    {
+        for (int j = 0; j < adjacencyList[i].size(); j++)
+        {
+            csr.colIdx.push_back(adjacencyList[i][j].first);
+            csr.values.push_back(adjacencyList[i][j].second);
+        }
+
+        csr.rowPtr.push_back(csr.colIdx.size());
+    }
+
+    return csr;
+}
