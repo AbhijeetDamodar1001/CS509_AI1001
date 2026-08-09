9147308e839439769bda1b68fc0f6842ab91e408
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-09 22:59:14 +0530

    Implement Floyd-Warshall algorithm in C++

---
+++ assignment_02/src/floydwarshall.cpp
@@ -0,0 +1,43 @@
+#include "floydwarshall.h"
+
+using namespace std;
+
+void floydWarshall(
+    vector<vector<int>>& distance,
+    bool& negativeCycle
+)
+{
+    int vertices = distance.size();
+
+    negativeCycle = false;
+
+    // Try every vertex as an intermediate vertex
+    for (int k = 0; k < vertices; k++)
+    {
+        for (int i = 0; i < vertices; i++)
+        {
+            for (int j = 0; j < vertices; j++)
+            {
+                if (distance[i][k] != 1000000000 &&
+                    distance[k][j] != 1000000000)
+                {
+                    if (distance[i][k] + distance[k][j] < distance[i][j])
+                    {
+                        distance[i][j] =
+                            distance[i][k] + distance[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+    // Check diagonal for a negative cycle
+    for (int i = 0; i < vertices; i++)
+    {
+        if (distance[i][i] < 0)
+        {
+            negativeCycle = true;
+            break;
+        }
+    }
+}
