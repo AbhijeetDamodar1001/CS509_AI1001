19f32309e30ed4d8f77875c897463cdb94d7788a
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-11 22:41:56 +0530

    Update bellmanford.cpp

--- assignment_02/src/bellmanford.cpp
+++ assignment_02/src/bellmanford.cpp
@@ -1,6 +1,6 @@
 #include "bellmanford.h"
 
-#include <vector>    // Input file driver.cpp mein dali hai
+#include <vector>    
 #include <climits>
 
 using namespace std;
@@ -10,16 +10,15 @@ vector<int> bellmanFord(const csrgraph &graph,
                         int source,
                         bool &negativeCycle)
 {
-    // Distance array
+    
     vector<int> distance(vertices, INT_MAX);
 
-    // Source vertex distance
     distance[source] = 0;
 
-    // Initially assume no negative cycle
+    
     negativeCycle = false;
 
-    // Relax all edges (V-1) times
+    
     for(int i = 0; i < vertices - 1; i++)
     {
         bool updated = false;
@@ -40,14 +39,13 @@ vector<int> bellmanFord(const csrgraph &graph,
             }
         }
 
-        // Stop early if no update happened
+        
         if(!updated)
         {
             break;
         }
     }
 
-    // Check for negative weight cycle
     for(int u = 0; u < vertices; u++)
     {
         for(int j = graph.rowPtr[u]; j < graph.rowPtr[u + 1]; j++)
